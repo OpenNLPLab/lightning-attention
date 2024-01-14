eb45223395a316faa70ec575166dcbf049208e62
Doraemonzzz <715073608@qq.com>
2024-01-14 03:08:03 +0000

    reformat setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="lightning_attn",
+    packages=find_packages(
+        exclude=[
+            "tests",
+            "benchmarks",
+        ]
+    ),
+    install_requires=["torch", "einops", "triton"],
+    version="0.0.0",
+    author="Doraemonzzz",
+    include_package_data=True,
+)
