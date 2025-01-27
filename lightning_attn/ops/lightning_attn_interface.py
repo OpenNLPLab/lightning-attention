OpenNLPLab/lightning-attention

commit 0ae41be8808ee5acaefa9bb6eaf209c116c7ccf7

format code

lightning_attn/ops/lightning_attn_interface.py
@@ -2,7 +2,7 @@ import math
 
 import torch.nn.functional as F
 
-from .triton import lightning_attn2
+from .triton import lightning_attn2, lightning_attn2_no_decay
 
 
 def is_support(dim):
@@ -43,9 +43,15 @@ def lightning_attn_func(q, k, v, s=None):
             end = arr[i + 1]
             q1 = q[..., start:end]
             k1 = k[..., start:end]
-            o += lightning_attn2(q1, k1, v, s)
+            if s != None:
+                o += lightning_attn2(q1, k1, v, s)
+            else:
+                o += lightning_attn2_no_decay(q1, k1, v)
     else:
-        o = lightning_attn2(q, k, v, s)
+        if s != None:
+            o = lightning_attn2(q, k, v, s)
+        else:
+            o = lightning_attn2_no_decay(q1, k1, v)
 
     if need_pad:
         o = o[:, :, :, :e]

benchmarks/benchmark_lightning2.py
@@ -5,7 +5,7 @@ import torch
 import triton
 from einops import rearrange
 
-from lightning_attn.ops import lightning_attn2
+from lightning_attn.ops import lightning_attn2, lightning_attn2_no_decay
 from lightning_attn.utils import _build_slope_tensor, get_memory
 
 try:
@@ -62,10 +62,10 @@ speed_configs = [
         xlabel="Sequence Length",
         ylabel="Execution Time(ms)",
         line_arg="provider",
-        line_vals=["lightning2"]
+        line_vals=["lightning2", "lightning2_no_decay"]
         + (["flash2"] if HAS_FLASH else [])
         + (["xformers"] if HAS_XFORMERS else []),
-        line_names=["Lightning2"]
+        line_names=["Lightning2", "Lightning2NoDecay"]
         + (["Flash2"] if HAS_FLASH else [])
         + (["Xformers"] if HAS_XFORMERS else []),
         styles=[
@@ -103,6 +103,8 @@ def bench_speed(b, h, n, d, dtype, device, mode, provider):
 
     if provider == "lightning2":
         fn = lambda: lightning_attn2(q, k, v, s)
+    elif provider == "lightning2_no_decay":
+        fn = lambda: lightning_attn2_no_decay(q, k, v)
     elif provider == "flash2":
         fn = lambda: flash_wrapper(q, k, v)
     else:
@@ -126,10 +128,10 @@ memory_configs = [
         xlabel="Sequence Length",
         ylabel="Memory(mb)",
         line_arg="provider",
-        line_vals=["lightning2"]
+        line_vals=["lightning2", "lightning2_no_decay"]
         + (["flash2"] if HAS_FLASH else [])
         + (["xformers"] if HAS_XFORMERS else []),
-        line_names=["Lightning2"]
+        line_names=["Lightning2", "Lightning2NoDecay"]
         + (["Flash2"] if HAS_FLASH else [])
         + (["Xformers"] if HAS_XFORMERS else []),
         styles=[
@@ -139,7 +141,7 @@ memory_configs = [
             ("blue", "-"),
             ("black", "-"),
         ],
-        plot_name=f"lightning2-speed_{mode}-batch{b}-head{h}-qk_dim{d}-v_dim{e}-dtype_{dtype_name}",
+        plot_name=f"lightning2-memory_{mode}-batch{b}-head{h}-qk_dim{d}-v_dim{e}-dtype_{dtype_name}",
         args={
             "b": b,
             "h": h,
@@ -166,6 +168,8 @@ def bench_memory(b, h, n, d, dtype, device, mode, provider):
 
     if provider == "triton":
         fn = lambda: lightning_attn2(q, k, v, s)
+    elif provider == "lightning2_no_decay":
+        fn = lambda: lightning_attn2_no_decay(q, k, v)
     elif provider == "flash":
         fn = lambda: flash_wrapper(q, k, v)
     else:
@@ -191,5 +195,5 @@ def bench_memory(b, h, n, d, dtype, device, mode, provider):
 
 save_path = "stat/lightning2"
 os.makedirs(save_path, exist_ok=True)
-# bench_speed.run(save_path=save_path, print_data=True)
+bench_speed.run(save_path=save_path, print_data=True)
 bench_memory.run(save_path=save_path, print_data=True)

lightning_attn/ops/torch/linear_attention.py
@@ -13,15 +13,18 @@ def get_mask(n, slope=1):
 
 
 def get_full_mask(n, slopes):
-    arr = []
-    for slope in slopes:
-        arr.append(get_mask(n, slope.item()))
-    mask = torch.stack(arr, dim=0)
+    if slopes == None:
+        mask = torch.tril(torch.ones((n, n)))
+    else:
+        arr = []
+        for slope in slopes:
+            arr.append(get_mask(n, slope.item()))
+        mask = torch.stack(arr, dim=0)
 
     return mask
 
 
-def linear_attn(q, k, v, s):
+def linear_attn(q, k, v, s=None):
     b, h, n, d = q.shape
     mask = get_full_mask(n, s).to(q.device).to(torch.float32)
     qk = torch.matmul(q, k.transpose(2, 3))

lightning_attn/ops/triton/__init__.py
@@ -1,2 +1,3 @@
 from .lightning_attn2 import *
+from .lightning_attn2_no_decay import lightning_attn2_no_decay
 from .srmsnorm import *

lightning_attn/ops/triton/lightning_attn2_no_decay.py
@@ -1,0 +1,471 @@
+# Copyright (c) 2024 Doraemonzzz
+
+import torch
+import triton
+import triton.language as tl
+
+
+@triton.jit
+def _fwd_kernel(
+    Q,
+    K,
+    V,
+    Out,
+    b: tl.constexpr,
+    h: tl.constexpr,
+    n: tl.constexpr,
+    d: tl.constexpr,
+    e: tl.constexpr,
+    BLOCK: tl.constexpr,
+    NUM_BLOCK: tl.constexpr,
+    BLOCK_MODEL: tl.constexpr,
+):
+    ##### get offset
+    off_bh = tl.program_id(0)
+    off_bh % h
+    off_e = tl.program_id(1)
+    qk_offset = off_bh * n * d
+    v_offset = off_bh * n * e
+    o_offset = off_bh * n * e
+    # channel offset
+    e_offset = off_e * BLOCK_MODEL
+
+    ##### get block ptr
+    Q_block_ptr = Q + qk_offset + tl.arange(0, d)[None, :]
+    K_trans_block_ptr = K + qk_offset + tl.arange(0, d)[:, None]
+    V_block_ptr = V + v_offset + e_offset + tl.arange(0, BLOCK_MODEL)[None, :]
+    O_block_ptr = Out + o_offset + e_offset + tl.arange(0, BLOCK_MODEL)[None, :]
+
+    ##### init diag decay(Lambda); q, k decay; kv
+    # q, k decay
+    off_block = tl.arange(
+        0, BLOCK
+    )  # Not bug, this is a bit different from algorithm 1, but is mathematically equivalent
+    # diag decay
+    index = off_block[:, None] - off_block[None, :]
+    kv = tl.zeros([d, BLOCK_MODEL], dtype=tl.float32)
+
+    ##### compute
+    for i in range(NUM_BLOCK):
+        # load
+        q = tl.load(
+            Q_block_ptr + off_block[:, None] * d, mask=off_block[:, None] < n, other=0.0
+        ).to(tl.float32)
+        k_trans = tl.load(
+            K_trans_block_ptr + off_block[None, :] * d,
+            mask=off_block[None, :] < n,
+            other=0.0,
+        ).to(tl.float32)
+        v = tl.load(
+            V_block_ptr + off_block[:, None] * e, mask=off_block[:, None] < n, other=0.0
+        ).to(tl.float32)
+
+        # compute
+        qk = tl.dot(q, k_trans)
+        qk = tl.where(index >= 0, qk, 0)
+        o_intra = tl.dot(qk, v)
+        o_inter = tl.dot(q, kv)
+        o = o_intra + o_inter
+
+        # save and update
+        tl.store(
+            O_block_ptr + off_block[:, None] * e,
+            o.to(O_block_ptr.dtype.element_ty),
+            mask=off_block[:, None] < n,
+        )
+        kv += tl.dot(k_trans, v)
+        off_block += BLOCK
+
+
+@triton.jit
+def _bwd_intra_kernel(
+    Q,
+    K,
+    V,
+    DO,
+    DQ,
+    DK,
+    DV,
+    b: tl.constexpr,
+    h: tl.constexpr,
+    n: tl.constexpr,
+    d: tl.constexpr,
+    e: tl.constexpr,
+    BLOCK: tl.constexpr,
+    NUM_BLOCK: tl.constexpr,
+    CBLOCK: tl.constexpr,
+    NUM_CBLOCK: tl.constexpr,
+):
+    ##### get offset
+    off_bh = tl.program_id(0)
+    off_block = tl.program_id(1)
+    off_bh % h
+    qk_offset = off_bh * n * d
+    v_offset = off_bh * n * e
+    o_offset = off_bh * n * e
+    block_offset = off_block * BLOCK + tl.arange(0, BLOCK)
+
+    ##### get block ptr
+    Q_trans_block_ptr = (
+        Q + qk_offset + block_offset[None, :] * d + tl.arange(0, d)[:, None]
+    )
+    K_block_ptr = K + qk_offset + block_offset[:, None] * d + tl.arange(0, d)[None, :]
+    V_trans_block_ptr = (
+        V + v_offset + block_offset[None, :] * e + tl.arange(0, e)[:, None]
+    )
+
+    DQ_block_ptr = DQ + qk_offset + block_offset[:, None] * d + tl.arange(0, d)[None, :]
+    DK_trans_block_ptr = (
+        DK + qk_offset + block_offset[None, :] * d + tl.arange(0, d)[:, None]
+    )
+    DV_block_ptr = DV + v_offset + block_offset[:, None] * e + tl.arange(0, e)[None, :]
+    DO_block_ptr = DO + o_offset + block_offset[:, None] * e + tl.arange(0, e)[None, :]
+
+    ##### init diag decay(Lambda)
+    array = tl.arange(0, BLOCK).to(tl.float32)
+    # diag
+    index = array[:, None] - array[None, :]
+
+    ##### load block
+    k = tl.load(K_block_ptr, mask=block_offset[:, None] < n, other=0.0).to(tl.float32)
+    v_trans = tl.load(V_trans_block_ptr, mask=block_offset[None, :] < n, other=0.0).to(
+        tl.float32
+    )
+    do = tl.load(DO_block_ptr, mask=block_offset[:, None] < n, other=0.0).to(tl.float32)
+    q_trans = tl.load(Q_trans_block_ptr, mask=block_offset[None, :] < n, other=0.0).to(
+        tl.float32
+    )
+
+    ##### compute
+    dqk = tl.dot(do, v_trans)
+    dqk = tl.where(index >= 0, dqk, 0)
+    dq_intra = tl.dot(dqk, k)
+
+    dk_intra_trans = tl.dot(q_trans, dqk)
+
+    qk_trans = tl.dot(k, q_trans)
+    qk_trans = tl.where(index <= 0, qk_trans, 0)
+    dv_intra = tl.dot(qk_trans, do)
+
+    dq = dq_intra
+    dk_trans = dk_intra_trans
+    dv = dv_intra
+
+    # save
+    tl.store(
+        DQ_block_ptr,
+        dq.to(DQ_block_ptr.dtype.element_ty),
+        mask=block_offset[:, None] < n,
+    )
+    tl.store(
+        DK_trans_block_ptr,
+        dk_trans.to(DK_trans_block_ptr.dtype.element_ty),
+        mask=block_offset[None, :] < n,
+    )
+    tl.store(
+        DV_block_ptr,
+        dv.to(DV_block_ptr.dtype.element_ty),
+        mask=block_offset[:, None] < n,
+    )
+
+
+@triton.jit
+def _bwd_inter_kernel(
+    Q,
+    K,
+    V,
+    DO,
+    DQ,
+    DK,
+    DV,
+    b: tl.constexpr,
+    h: tl.constexpr,
+    n: tl.constexpr,
+    d: tl.constexpr,
+    e: tl.constexpr,
+    BLOCK: tl.constexpr,
+    NUM_BLOCK: tl.constexpr,
+    CBLOCK: tl.constexpr,
+    NUM_CBLOCK: tl.constexpr,
+):
+    ##### get offset
+    off_bh = tl.program_id(0)
+    off_bh % h
+
+    qk_offset = off_bh * n * d
+    v_offset = off_bh * n * e
+    o_offset = off_bh * n * e
+
+    ##### get block ptr
+    DQ_block_ptr = (
+        DQ + qk_offset + tl.arange(0, CBLOCK)[:, None] * d + tl.arange(0, d)[None, :]
+    )
+    K_block_ptr = (
+        K + qk_offset + tl.arange(0, CBLOCK)[:, None] * d + tl.arange(0, d)[None, :]
+    )
+    V_trans_block_ptr = (
+        V + v_offset + tl.arange(0, CBLOCK)[None, :] * e + tl.arange(0, e)[:, None]
+    )
+    DO_block_ptr = (
+        DO + o_offset + tl.arange(0, CBLOCK)[:, None] * e + tl.arange(0, e)[None, :]
+    )
+    # mask
+    off_block1 = tl.arange(0, CBLOCK)
+    off_block2 = tl.arange(0, CBLOCK)
+
+    ##### init lambda; kv
+    kv_trans = tl.zeros([e, d], dtype=tl.float32)
+
+    ##### compute dq inter
+    for i in range(NUM_BLOCK):
+        # compute in subblock
+        for j in range(NUM_CBLOCK):
+            if i > 0:  # if not add this, may have bug
+                do = tl.load(DO_block_ptr, mask=off_block1[:, None] < n, other=0.0).to(
+                    tl.float32
+                )
+                dq_inter = tl.dot(do, kv_trans)
+                dq = dq_inter + tl.load(
+                    DQ_block_ptr, mask=off_block1[:, None] < n, other=0.0
+                )
+                tl.store(
+                    DQ_block_ptr,
+                    dq.to(DQ_block_ptr.dtype.element_ty),
+                    mask=off_block1[:, None] < n,
+                )
+
+            DQ_block_ptr += CBLOCK * d
+            DO_block_ptr += CBLOCK * e
+            off_block1 += CBLOCK
+
+        # update kv in subblock
+        kv_trans_current = tl.zeros([e, d], dtype=tl.float32)
+        for j in range(NUM_CBLOCK):
+            v_trans = tl.load(
+                V_trans_block_ptr, mask=off_block2[None, :] < n, other=0.0
+            ).to(tl.float32)
+            k = tl.load(K_block_ptr, mask=off_block2[:, None] < n, other=0.0).to(
+                tl.float32
+            )
+            kv_trans_current += tl.dot(v_trans, k)
+
+            K_block_ptr += CBLOCK * d
+            V_trans_block_ptr += CBLOCK * e
+            off_block2 += CBLOCK
+
+        kv_trans += kv_trans_current
+
+    ##### get block ptr
+    m = NUM_BLOCK * BLOCK
+    off_block1 = m + tl.arange(0, CBLOCK)
+    off_block2 = m + tl.arange(0, CBLOCK)
+
+    Q_trans_block_ptr = (
+        Q
+        + qk_offset
+        + m * d
+        + tl.arange(0, CBLOCK)[None, :] * d
+        + tl.arange(0, d)[:, None]
+    )
+    K_block_ptr = (
+        K
+        + qk_offset
+        + m * d
+        + tl.arange(0, CBLOCK)[:, None] * d
+        + tl.arange(0, d)[None, :]
+    )
+    V_trans_block_ptr = (
+        V
+        + v_offset
+        + m * e
+        + tl.arange(0, CBLOCK)[None, :] * e
+        + tl.arange(0, e)[:, None]
+    )
+
+    DK_trans_block_ptr = (
+        DK
+        + qk_offset
+        + m * d
+        + tl.arange(0, CBLOCK)[None, :] * d
+        + tl.arange(0, d)[:, None]
+    )
+    DV_block_ptr = (
+        DV
+        + v_offset
+        + m * e
+        + tl.arange(0, CBLOCK)[:, None] * e
+        + tl.arange(0, e)[None, :]
+    )
+    DO_block_ptr = (
+        DO
+        + o_offset
+        + m * e
+        + tl.arange(0, CBLOCK)[:, None] * e
+        + tl.arange(0, e)[None, :]
+    )
+
+    ##### init dkv
+    dkv = tl.zeros([d, e], dtype=tl.float32)
+
+    ##### compute dk, dv inter
+    for i in range(NUM_BLOCK - 1, -1, -1):
+        # compute in subblock
+        for j in range(NUM_CBLOCK - 1, -1, -1):
+            K_block_ptr -= CBLOCK * d
+            V_trans_block_ptr -= CBLOCK * e
+            DK_trans_block_ptr -= CBLOCK * d
+            DV_block_ptr -= CBLOCK * e
+            off_block1 -= CBLOCK
+
+            if i < NUM_BLOCK - 1:  # if not add this, may have bug
+                k = tl.load(K_block_ptr, mask=off_block1[:, None] < n, other=0.0).to(
+                    tl.float32
+                )
+                v_trans = tl.load(
+                    V_trans_block_ptr, mask=off_block1[None, :] < n, other=0.0
+                ).to(tl.float32)
+
+                dk_inter_trans = tl.dot(dkv, v_trans)
+                dv_inter = tl.dot(k, dkv)
+
+                dk_trans = dk_inter_trans + tl.load(
+                    DK_trans_block_ptr, mask=off_block1[None, :] < n, other=0.0
+                )
+                dv = dv_inter + tl.load(
+                    DV_block_ptr, mask=off_block1[:, None] < n, other=0.0
+                )
+
+                tl.store(
+                    DK_trans_block_ptr,
+                    dk_trans.to(DK_trans_block_ptr.dtype.element_ty),
+                    mask=off_block1[None, :] < n,
+                )
+                tl.store(
+                    DV_block_ptr,
+                    dv.to(DV_block_ptr.dtype.element_ty),
+                    mask=off_block1[:, None] < n,
+                )
+
+        # update dkv in subblock
+        dkv_current = tl.zeros([d, e], dtype=tl.float32)
+        for j in range(NUM_CBLOCK - 1, -1, -1):
+            DO_block_ptr -= CBLOCK * e
+            Q_trans_block_ptr -= CBLOCK * d
+            off_block2 -= CBLOCK
+
+            do = tl.load(DO_block_ptr, mask=off_block2[:, None] < n, other=0.0).to(
+                tl.float32
+            )
+            q_trans = tl.load(
+                Q_trans_block_ptr, mask=off_block2[None, :] < n, other=0.0
+            ).to(tl.float32)
+            dkv_current += tl.dot(q_trans, do)
+
+        dkv += dkv_current
+
+
+class LightningAttention2NoDecay(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, q, k, v):
+        q = q.contiguous()
+        k = k.contiguous()
+        v = v.contiguous()
+
+        b, h, n, d = q.shape
+        e = v.shape[-1]
+        o = torch.empty((b, h, n, e), dtype=q.dtype, device=q.device)
+
+        BLOCK = 64
+        NUM_BLOCK = triton.cdiv(q.shape[2], BLOCK)
+        # parallel over channel
+        BLOCK_MODEL = min(triton.next_power_of_2(e), 32)
+        grid = (b * h, triton.cdiv(e, BLOCK_MODEL))
+
+        _fwd_kernel[grid](
+            q,
+            k,
+            v,
+            o,
+            b,
+            h,
+            n,
+            d,
+            e,
+            BLOCK=BLOCK,
+            NUM_BLOCK=NUM_BLOCK,
+            BLOCK_MODEL=BLOCK_MODEL,
+        )
+
+        ctx.save_for_backward(q, k, v)
+
+        return o
+
+    @staticmethod
+    def backward(ctx, do):
+        q, k, v = ctx.saved_tensors
+
+        q = q.contiguous()
+        k = k.contiguous()
+        v = v.contiguous()
+        do = do.contiguous()
+
+        dq = torch.empty_like(q)
+        dk = torch.empty_like(k)
+        dv = torch.empty_like(v)
+
+        b, h, n, d = q.shape
+        e = v.shape[-1]
+
+        # block size
+        BLOCK = 64
+        NUM_BLOCK = triton.cdiv(n, BLOCK)
+        # compute block size
+        CBLOCK = 32
+        NUM_CBLOCK = BLOCK // CBLOCK
+
+        # for intra part, compute in parallel
+        grid = (b * h, NUM_BLOCK)
+        _bwd_intra_kernel[grid](
+            q,
+            k,
+            v,
+            do,
+            dq,
+            dk,
+            dv,
+            b,
+            h,
+            n,
+            d,
+            e,
+            BLOCK=BLOCK,
+            NUM_BLOCK=NUM_BLOCK,
+            CBLOCK=CBLOCK,
+            NUM_CBLOCK=NUM_CBLOCK,
+        )
+
+        # for inter part, compute in sequencial
+        grid = (b * h,)
+        _bwd_inter_kernel[grid](
+            q,
+            k,
+            v,
+            do,
+            dq,
+            dk,
+            dv,
+            b,
+            h,
+            n,
+            d,
+            e,
+            BLOCK=BLOCK,
+            NUM_BLOCK=NUM_BLOCK,
+            CBLOCK=CBLOCK,
+            NUM_CBLOCK=NUM_CBLOCK,
+        )
+
+        return dq, dk, dv
+
+
+lightning_attn2_no_decay = LightningAttention2NoDecay.apply

tests/ops/test_lightning2_no_decay.py
@@ -1,0 +1,62 @@
+import pytest
+import torch
+
+from lightning_attn.ops import lightning_attn2_no_decay, linear_attn
+
+
+def get_params():
+    array = [
+        (6, 8, 256, 128, 64),
+        (6, 8, 512, 128, 64),
+        (6, 8, 1024, 128, 64),
+        (6, 8, 2048, 128, 64),
+        (6, 8, 4096, 128, 64),
+        (6, 8, 8192, 128, 64),
+        (6, 8, 2048, 32, 64),
+        (6, 8, 2048, 64, 64),
+        (6, 12, 2048, 128, 64),
+        (6, 16, 2048, 128, 64),
+        (6, 20, 2048, 128, 64),
+        (1, 8, 2048, 128, 64),
+        (2, 8, 2048, 128, 64),
+        (3, 8, 2048, 128, 64),
+        (6, 8, 913, 128, 64),
+        (6, 8, 513, 128, 64),
+        (6, 8, 1213, 128, 64),
+        (6, 8, 2048, 16, 64),
+    ]
+
+    return array
+
+
+@pytest.mark.parametrize("b, h, n, d, e", get_params())
+@pytest.mark.parametrize("dtype", [torch.bfloat16, torch.float16])
+def test_lightning2(b, h, n, d, e, dtype):
+    torch.manual_seed(2024)
+    device = torch.device("cuda")
+    q = (torch.randn((b, h, n, d), dtype=dtype, device=device) / 10).requires_grad_()
+    k = (torch.randn((b, h, n, d), dtype=dtype, device=device) / 10).requires_grad_()
+    v = (torch.randn((b, h, n, e), dtype=dtype, device=device) / 10).requires_grad_()
+    do = torch.randn((b, h, n, e), dtype=dtype, device=device) / 10
+    s = None
+
+    # forward
+    o_ref = linear_attn(q, k, v, s)
+    o = lightning_attn2_no_decay(q, k, v)
+
+    # backward
+    o_ref.backward(do, retain_graph=True)
+    dq_ref, q.grad = q.grad.clone(), None
+    dk_ref, k.grad = k.grad.clone(), None
+    dv_ref, v.grad = v.grad.clone(), None
+
+    o.backward(do, retain_graph=True)
+    dq, q.grad = q.grad.clone(), None
+    dk, k.grad = k.grad.clone(), None
+    dv, v.grad = v.grad.clone(), None
+
+    print(torch.norm(o - o_ref))
+    print(torch.norm(dq - dq_ref))
+    print(torch.norm(dk - dk_ref))
+    print(torch.norm(dv - dv_ref))
+    assert False

tests/script.sh
@@ -3,6 +3,7 @@ date=$(date '+%Y-%m-%d-%H:%M:%S')
 folder=ops
 file=test_srmsnorm
 file=test_lightning2
+file=test_lightning2_no_decay
 
 mkdir -p $folder/log
 

commit 80188c2a5ef1290c4a98405b19395c389270890b

Fix typo in lightning_attn_interface.py
Introduced in 0ae41be

The `k1, v1` inputs look like they were copied from the `if d > 128` case.

lightning_attn/ops/lightning_attn_interface.py
@@ -50,7 +50,7 @@ def lightning_attn_func(q, k, v, s=None):
         if s != None:
             o = lightning_attn2(q, k, v, s)
         else:
-            o = lightning_attn2_no_decay(q1, k1, v)
+            o = lightning_attn2_no_decay(q, k, v)
 
     if need_pad:
         o = o[:, :, :, :e]